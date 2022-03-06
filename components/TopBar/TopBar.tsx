fe4b4b258c4998f69631d1a282b50ea34b792c68
Nitzan Hen <code@nitzanhen.net>
2022-03-06 19:56:54 +0200

    Add home link to top bar

--- components/TopBar/TopBar.tsx
+++ components/TopBar/TopBar.tsx
@@ -1,6 +1,7 @@
 import classes from './top-bar.module.scss';
 import Icon from '@mdi/react';
 import { mdiGithub } from '@mdi/js';
+import Link from 'next/link';
 
 export interface TopBarProps { }
 
@@ -8,7 +9,9 @@ export const TopBar: React.VFC<TopBarProps> = () => {
 
 	return (
 		<header className={classes.header}>
-			<span>Rhax</span>
+			<Link href='/'>
+				<a>Rhax</a>	
+			</Link>
 			<a href='https://github.com/NitzanHen/rhax' target='_blank' rel="noreferrer">
 				<Icon path={mdiGithub} color='var(--color-text-white)' />
 			</a>
